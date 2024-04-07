9307562225494f6051b5072963a303eddfbadd0f
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-04-07 22:23:51 +0300

    update

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,21 +1,19 @@
 import Axios from 'axios';
 
 export async function getImages(query, currentPage) {
-    const axios = Axios.create({
-        baseURL: 'https://pixabay.com/api/',
-        params: {
-            key: '43067029-c55b458b38852b6054b39e36e',
-            q: query;
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: true,
-            page: currentPage,
-            per_page: 15,
-            },
-    });
-    
-    const responce = await axios.get('');
-    return responce.data;
+  const axios = Axios.create({
+    baseURL: 'https://pixabay.com/api/',
+    params: {
+      key: '43067029-c55b458b38852b6054b39e36e',
+      q: query,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      page: currentPage,
+      per_page: 15,
+    },
+  });
 
+  const responce = await axios.get('');
+  return responce.data;
 }
-
