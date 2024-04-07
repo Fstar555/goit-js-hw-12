813a83bf1b02d6b8f6886d3fc7b04f1ea711e2a0
Fstar555 <133888089+Fstar555@users.noreply.github.com>
2024-04-07 15:14:27 +0300

    update js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -18,3 +18,4 @@ export async function getImages(query, currentPage) {
     return responce.data;
 
 }
+
---
+++ src/js/render-function.js
@@ -0,0 +1,51 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+const lightbox = new SimpleLightbox('.gallery-link', {
+  captionData: 'alt',
+  captionDelay: 250,
+});
+
+export const galeryEl = document.querySelector('.gallery');
+
+function imageTemplate(obj) {
+  const {
+    largeImageURL,
+    webformatURL,
+    tags,
+    likes,
+    views,
+    comments,
+    downloads,
+  } = obj;
+  return `<li class="gallery-item">
+  <a class="gallery-link"
+    href="${largeImageURL}"
+    ><img class="img" loading="lazy"
+      src="${webformatURL}"
+      alt="${tags}"
+  />
+  <ul class="img-dscr">
+    <li>
+      <p><b>Likes</b><br /> ${likes}</p>
+    </li>
+    <li>
+      <p><b>Views</b><br /> ${views}</p>
+    </li>
+    <li>
+      <p><b>Comments</b><br /> ${comments}</p>
+    </li>
+    <li>
+      <p><b>Downloads</b><br /> ${downloads}</p>
+    </li>
+  </ul>
+  </a>
+</li>
+`;
+}
+
+export function createMarkUp(arr) {
+  const markup = imageTemplate(arr);
+  galeryEl.insertAdjacentElement('beforeend', markup);
+  lightbox.refresh();
+}
